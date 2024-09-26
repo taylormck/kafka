a2705fa5eb941e6700c83d3c46881feb059aef9b
Taylor McKinney <2374670+taylormck@users.noreply.github.com>
2024-09-26 18:38:23 +0900

    feat: respond to requests with hard-coded integer

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,16 @@
 #![allow(unused_imports)]
-use std::net::TcpListener;
+use std::{io::Write, net::TcpListener};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:9092").unwrap();
 
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
-                println!("accepted a connection");
+            Ok(mut stream) => {
+                println!("accepted new connection");
+
+                let buf: [u8; 8] = [0, 0, 0, 8, 0, 0, 0, 7];
+                stream.write_all(&buf).unwrap();
             }
             Err(e) => {
                 println!("error: {}", e);
