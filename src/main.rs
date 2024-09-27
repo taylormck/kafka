11a7aa786ac11c758a9cced9519e88203c5acd00
Taylor McKinney <2374670+taylormck@users.noreply.github.com>
2024-09-28 00:31:43 +0900

    feat: implement first part of fetch response

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 #![allow(unused_imports)]
-use bytes::{Buf, BufMut};
+use bytes::{Buf, BufMut, Bytes};
 use num_derive::FromPrimitive;
 use num_traits::FromPrimitive;
 use std::{
@@ -11,19 +11,20 @@ use tokio::{
     net::TcpListener,
 };
 
-#[derive(Copy, Clone, Debug)]
+#[derive(Debug)]
 struct RequestHeader {
     api_key: ApiKey,
     api_version: i16,
     correlation_id: i32,
+    client_id: Bytes,
 }
 
-#[repr(i16)]
-enum ErrorCode {
+#[derive(Copy, Clone, Debug)]
+enum KafkaError {
     UnsupportedVersion = 35,
+    UnknownTopic = 100,
 }
 
-#[repr(i16)]
 #[derive(Copy, Clone, Debug, FromPrimitive)]
 enum ApiKey {
     ApiVersions = 18,
@@ -65,7 +66,12 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                     },
                     api_version: request.get_i16(),
                     correlation_id: request.get_i32(),
+                    client_id: {
+                        let client_id_len: usize = request.get_u16().into();
+                        request.copy_to_bytes(client_id_len)
+                    },
                 };
+                request.advance(1); // TAG_BUFFER
 
                 let mut body = Vec::new();
                 body.put_i32(header.correlation_id);
@@ -73,7 +79,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                 match header.api_key {
                     ApiKey::ApiVersions => {
                         let error_code = match !(0..=4).contains(&header.api_version) {
-                            true => ErrorCode::UnsupportedVersion as i16,
+                            true => KafkaError::UnsupportedVersion as i16,
                             false => 0_i16,
                         };
                         body.put_i16(error_code);
@@ -96,22 +102,87 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                         body.put_i8(0); // TAG_BUFFER length
                     }
                     ApiKey::Fetch => {
+                        // Read request
+                        let _max_wait_ms = request.get_i32();
+                        let _min_bytes = request.get_i32();
+                        let _max_bytes = request.get_i32();
+                        let _isolation_level = request.get_i8();
+                        let _session_id = request.get_i32();
+                        let _session_epoch = request.get_i32();
+
+                        let topic_count = request.get_u8().saturating_sub(1);
+                        let mut topics = Vec::with_capacity(topic_count.into());
+
+                        for _ in 0..topic_count {
+                            // topic_id is actually a UUID, but we don't need
+                            // to decode it, so we just treat it like a big int.
+                            let topic_id = request.get_u128();
+
+                            let partition_count = request.get_u8().saturating_sub(1);
+
+                            // NOTE: we'll need this once we start processing the data.
+                            // let mut partitions = Vec::with_capacity(partition_count);
+
+                            for _ in 0..partition_count {
+                                let _partition = request.get_i32();
+                                let _current_leader_epoch = request.get_i32();
+                                let _fetch_offset = request.get_i64();
+                                let _last_fetched_epoch = request.get_i32();
+                                let _log_start_offset = request.get_i64();
+                                let _partition_max_bytes = request.get_i32();
+                                request.advance(1); // TAG_BUFFER
+                            }
+                            request.advance(1); // TAG_BUFFER
+
+                            topics.push(topic_id);
+                        }
+
+                        let forgotten_topics_data_count = request.get_u8().saturating_sub(1);
+
+                        for _ in 0..forgotten_topics_data_count {
+                            let _topic_id = request.get_u128();
+                            let _partitions = request.get_i32();
+                            request.advance(1); // TAG_BUFFER
+                        }
+
+                        let _rack_id_len = request.get_u8() - 1;
+                        request.advance(1); // TAG_BUFFER
+
                         body.put_u8(0); // TAG_BUFFER
                         body.put_i32(420); // throttle time in ms
 
                         let error_code = match !(0..=16).contains(&header.api_version) {
-                            true => ErrorCode::UnsupportedVersion as i16,
+                            true => KafkaError::UnsupportedVersion as i16,
                             false => 0_i16,
                         };
                         body.put_i16(error_code);
                         body.put_i32(0); // session_id
 
-                        body.put_u8(1); // n + 1 bytes follow
+                        body.put_u8(topics.len() as u8 + 1); // number of responses, but the tag buffer
+
+                        for topic_id in topics {
+                            body.put_u128(topic_id);
+
+                            // NOTE: these are dummy values for now.
+                            body.put_u8(2); // partitions
+                            body.put_u32(0); // index
+                            body.put_u16(KafkaError::UnknownTopic as u16);
+                            body.put_u64(0);
+                            body.put_u64(0);
+                            body.put_u64(0);
+                            body.put_u8(0);
+                            body.put_u32(0);
+                            body.put_u8(1);
+
+                            // Double TAG_BUFFER
+                            body.put_u8(0);
+                            body.put_u8(0);
+                        }
 
                         body.put_u8(0); // TAG_BUFFER
                     }
                     ApiKey::Produce => {
-                        todo!();
+                        // todo!();
                     }
                     ApiKey::None => {}
                 }
